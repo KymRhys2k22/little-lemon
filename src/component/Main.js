2786bf48b8dbb6a1b696c0692b8c5b8a2aa78101
KymRhys2k22 <kymrhys@gmail.com>
2023-08-05 01:00:04 +0800

    add component

--- src/component/Main.js
+++ src/component/Main.js
@@ -1,12 +1,16 @@
 import React from 'react'
+import HeroSection from './HeroSection'
+import Highlights from './Highlights'
+import Testimonials from './Testimonials'
+import About from './About'
 
 const Main = () => {
   return (
     <main>
-      Hero section
-      Highlights
-      Testimonials
-      About
+      <HeroSection/>
+      <Highlights/>
+      <Testimonials/>
+      <About/>
     </main>
   )
 }
